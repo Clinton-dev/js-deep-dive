5c54aab47ab53ff9fadb34475f53438488f9e8f6
Clinton-dev <clintonwambugu@gmail.com>
2023-07-04 10:49:45 +0300

    Display added notes to notes section

--- google-keep/app.js
+++ google-keep/app.js
@@ -1,9 +1,16 @@
 class App {
   constructor() {
+    this.notes = [];
+    this.title = "";
+    this.text = "";
+    this.id = "";
+
     this.$formContainer = document.querySelector("#form-container");
+    this.$form = document.querySelector("#form");
     this.$noteTitle = document.querySelector("#note-title");
     this.$noteText = document.querySelector("#note-text");
     this.$formButtons = document.querySelector("#form-buttons");
+    this.$notes = document.querySelector("#notes");
 
     this.handleEventListeners();
   }
@@ -12,14 +19,32 @@ class App {
     document.body.addEventListener("click", (event) => {
       this.handleFormClick(event);
     });
+
+    this.$form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      this.title = this.$noteTitle.value;
+      this.text = this.$noteText.value;
+      const hasContent = Boolean(this.title) || Boolean(this.text);
+      if (!hasContent) return;
+      this.createNote({ title: this.title, text: this.text });
+    });
   }
 
   handleFormClick(event) {
     const isFormClicked = this.$formContainer.contains(event.target);
-    console.log(isFormClicked);
+
     if (isFormClicked) {
       this.openForm();
     } else {
+      // save note to notes if user clicks outside of the form
+      this.title = this.$noteTitle.value;
+      this.text = this.$noteText.value;
+      const hasContent = Boolean(this.title) || Boolean(this.text);
+
+      if (hasContent) {
+        this.createNote({ title: this.title, text: this.text });
+      }
+
       this.closeForm();
     }
   }
@@ -34,6 +59,43 @@ class App {
     this.$formContainer.classList.remove("form-open");
     this.$noteTitle.style.display = "none";
     this.$formButtons.style.display = "none";
+  }
+
+  // TODO: Add note, display the notes in notes section, clear form
+  // check for notes length, if its greater than 1
+  // new id = previousNote.id + 1
+  // else there is exist no notes and id is 1
+  createNote(note) {
+    const noteId =
+      this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1;
+    const newNote = { ...note, backgroundColor: "white", id: noteId };
+
+    this.notes = [...this.notes, newNote];
+
+    this.$noteTitle.value = "";
+    this.$noteText.value = "";
+
+    this.displayNotes();
+  }
+
+  displayNotes() {
+    this.$notes.innerHTML = this.notes
+      .reverse()
+      .map(
+        (note) => `
+    <div style="background: ${note.backgroundColor};" class="note">
+          <div class="${note.title && "note-title"}">${note.title}</div>
+          <div class="note-text">${note.text}</div>
+          <div class="toolbar-container">
+            <div class="toolbar">
+              <img class="toolbar-color" src="/images/palette.svg">
+              <img class="toolbar-delete" src="/images/delete-icon.svg">
+            </div>
+          </div>
+        </div>
+    `
+      )
+      .join("");
   }
 }
 

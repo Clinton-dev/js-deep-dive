0ebfec424ffce6cb4632f818484a1fc38a0d27e2
Clinton-dev <clintonwambugu@gmail.com>
2023-07-11 11:25:25 +0300

    Add Comments component

--- y-combinator-clone/pages/item.js
+++ y-combinator-clone/pages/item.js
@@ -1,19 +1,25 @@
 import view from "../utils/view.js";
 import Story from "../components/Story.js";
+import Comment from "../components/Comment.js";
 import baseUrl from "../utils/baseUrl.js";
 
 export default async function Item() {
   const story = await getStory();
-  //   console.log(story);
+  const hasComments = story.comments.length > 0;
+
   view.innerHTML = `<div>
     ${Story(story)}
     <hr />
+    ${
+      hasComments
+        ? story.comments.map((comment) => Comment(comment)).join("")
+        : "No comments"
+    }
   </div>`;
 }
 
 async function getStory() {
   const id = window.location.hash.split("?id=")[1];
-  console.log(id);
   const url = `${baseUrl}/item/${id}`;
   const response = await fetch(url);
   const story = await response.json();
